5fee7c7f2b920f148876616dab1e6fed85402abd
Manoj Ganesan <manojganesan@gmail.com>
2021-12-22 22:05:00 -0800

    Day 3 Part 2

--- day3/day3_bin_diagnostic.py
+++ day3/day3_bin_diagnostic.py
@@ -29,13 +29,45 @@ def read_input(filename):
     return numbers_list
 
 
+def number_from_bits(bit_array):
+    numbers = 0
+    for bit in bit_array:
+        numbers = numbers << 1 | bit
+    return numbers
+
+
+def most_common(column_vector):
+    bincounts = np.bincount(column_vector)
+    assert len(bincounts) == 2
+    if bincounts[0] == bincounts[1]:
+        return -1
+    else:
+        return 0 if bincounts[0] > bincounts[1] else 1
+
+
+def trim(numbers, index, o2=True):
+    column_bits = numbers[:, index]  # look at first bit
+    max_bit = most_common(column_bits)
+    if max_bit == -1:
+        priority_bit = 1 if o2 else 0
+    else:
+        priority_bit = max_bit if o2 else 1 - max_bit
+    return numbers[np.where(column_bits == priority_bit)]
+
+
+def rating(numbers, o2=True):
+    for index in range(0, numbers[0].size):
+        numbers = trim(numbers, index, o2)
+        if len(numbers) == 1:
+            return number_from_bits(numbers[0])
+
+
 def get_rates(numbers):
     gamma_rate = 0
     epsilon_rate = 0
     for col_index in range(numbers[0].size):
         column_bits = numbers[:, col_index]
-        argmax_index = np.bincount(column_bits).argmax()
-        max_bit = argmax_index  # bin number
+        max_bit = most_common(column_bits)
         gamma_rate = gamma_rate << 1 | max_bit
         min_bit = 1 - max_bit
         epsilon_rate = epsilon_rate << 1 | min_bit
@@ -43,5 +75,12 @@ def get_rates(numbers):
     return gamma_rate, epsilon_rate
 
 
-gamma_rate, epsilon_rate = get_rates(np.asarray(read_input('day3_long_input.txt')))
+# part 2
+numbers = np.asarray(read_input('day3_long_input.txt'))
+o2_rating = rating(numbers)
+co2_rating = rating(numbers, o2=False)
+print(o2_rating * co2_rating)
+
+# part 1
+gamma_rate, epsilon_rate = get_rates(np.asarray(read_input('day3_short_input.txt')))
 print(gamma_rate * epsilon_rate)
